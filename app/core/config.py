00c48f5dfaf7cec876e6f2301c2d3c91c8e8dfb3
Nogueira7 <105489863+Nogueira7@users.noreply.github.com>
2026-05-03 13:53:56 +0100

    Update config.py

--- app/core/config.py
+++ app/core/config.py
@@ -11,9 +11,7 @@ ALGORITHM = os.getenv("ALGORITHM", "HS256")
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 60))
 REFRESH_TOKEN_EXPIRE_DAYS = int(os.getenv("REFRESH_TOKEN_EXPIRE_DAYS", 7))
 
-# --------------------------------------------------
 # DATABASE
-# --------------------------------------------------
 
 DB_HOST = os.getenv("DB_HOST", "localhost")
 DB_PORT = int(os.getenv("DB_PORT", 3306))
